6757214dbe16ab06ecdc418e5d8ad541dfbb917f
pavel-glazkov <109760316+pavel-glazkov@users.noreply.github.com>
2022-08-18 22:14:00 +0200

    Update Task2.py

--- Task2.py
+++ Task2.py
@@ -1,5 +1,8 @@
 from selenium import webdriver
+import time
 driver = webdriver.Chrome()
+driver.implicitly_wait(20)
+driver.maximize_window()
 driver.get('https://www.alza.cz/')
 
 cook = driver.find_element("xpath",'//*[@id="rootHtml"]/body/alza-component-head/div[6]/div/div/div[2]/a[1]')
@@ -22,14 +25,13 @@ price.click()
 
 first = driver.find_element("xpath",'//*[@id="boxes"]/div[1]/div[2]/div[1]/span/div/div/a')
 first.click()
-driver.implicitly_wait(20)
 
 goBack = driver.find_element("xpath",'//*[@id="varBBackButton"]')
 goBack.click()
 
 second = driver.find_element("xpath",'//*[@id="boxes"]/div[2]/div[2]/div[1]/span/div/div/a')
 second.click()
-driver.implicitly_wait(20)
+
 goToCart = driver.find_element("xpath",'//*[@id="varBToBasketButton"]')
 goToCart.click()
 
