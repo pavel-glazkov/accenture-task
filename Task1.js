556823a3aa75b371d093d16c7e26f88ab7431126
pavel-glazkov <109760316+pavel-glazkov@users.noreply.github.com>
2022-08-18 22:17:46 +0200

    Update Task1.js

--- Task1.js
+++ Task1.js
@@ -1,3 +1,10 @@
+//Write a program in Python or Java that counts backwards from 100 to 1 and prints: 
+//“Agile” if the number is divisible by 5, “Software” if the number is divisible by 3, 
+//“Testing” if the number is divisible by both, or prints just the number if none of those cases are true.
+
+//The code is written on JavaScript
+//To run the code
+
 let number = 100
 
 for (i=number; i>0; i--) {
